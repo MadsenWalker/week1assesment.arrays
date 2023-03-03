549fcd1e56aa11a52f8801d6143520d3c8bde5bc
MadsenWalker <madsenwalker@gmail.com>
2023-03-03 12:44:49 -0700

    assessment.sdlc

--- sdlc.js
+++ sdlc.js
@@ -12,6 +12,14 @@
 // Testing & Integration
 // Planning
 
+// Planning
+// Analysis of Requirements
+// Design
+// Testing & Integration
+// Implementation
+// Maintain
+
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +27,9 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+let analysisOfRequirements = "Creating a formal definition of what is required to a detail level enough for Design to carry on."
+let design = "defining the technical details of the product. Including but not limited to, sketches, system interfaces, and prototypes. Clients will use all these items to make final product decisions"
+let testingAndIntegration = "Construction of all components has been completed and is then tested. With successful testing, integration plans will be created including a transition plan for exisiting data"
+let Implementation = "After being successfully tested and accepted, the system is installed in its target environment"
+let Maintain = "software that has been implemented is now monitored. Regular repairs and upgrades are made as needed"
+
